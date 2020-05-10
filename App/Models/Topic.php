a8897aa0ebfee31bd5d7ea35127d495121db056b
Linh Nguyen <qlinh120198@gmail.com>
2020-05-10 12:01:11 +0700

    Add model topic

--- App/Models/Topic.php
+++ App/Models/Topic.php
@@ -28,8 +28,66 @@ class User extends \Core\Model
         }
     }
 
-    // create( -, -, -,)
-    // getOne(id)
-    // updateOne(id)
-    // deleteOne(id)
+    public static function create($id, $name, $description)
+    {
+        //$host = 'localhost';
+        //$dbname = 'mvc';
+        //$username = 'root';
+        //$password = 'secret';
+
+        try {
+            //$db = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+            $db = static::getDB();
+
+            $stmt = $db->query('INSERT INTO topic ('id','name','description') VALUES ('$id','$name','$description')');
+            $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+            return $results;
+
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+    }
+
+    public static function updateOne($id, $name, $description)
+    {
+        //$host = 'localhost';
+        //$dbname = 'mvc';
+        //$username = 'root';
+        //$password = 'secret';
+
+        try {
+            //$db = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+            $db = static::getDB();
+
+            $stmt = $db->query('UPDATE topic SET name = '$name', description = '$description' WHERE id = $id');
+            $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+            return $results;
+
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+    }
+    
+    public static function deleteOne($id)
+    {
+        //$host = 'localhost';
+        //$dbname = 'mvc';
+        //$username = 'root';
+        //$password = 'secret';
+
+        try {
+            //$db = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+            $db = static::getDB();
+
+            $stmt = $db->query('DELETE FROM topic WHERE id = $id');
+            $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+            return $results;
+
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+    }
 }
