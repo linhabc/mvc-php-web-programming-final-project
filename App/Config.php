8ee40c5c7bc33e4fede31fb5e71d946fe478e66d
linh nguyen <jonathanrocrach2@gmail.com>
2020-05-10 15:09:49 +0700

    user model

--- App/Config.php
+++ App/Config.php
@@ -9,34 +9,13 @@ namespace App;
  */
 class Config
 {
-
-    /**
-     * Database host
-     * @var string
-     */
     const DB_HOST = 'localhost';
 
-    /**
-     * Database name
-     * @var string
-     */
     const DB_NAME = 'mvc';
 
-    /**
-     * Database user
-     * @var string
-     */
     const DB_USER = 'root';
 
-    /**
-     * Database password
-     * @var string
-     */
     const DB_PASSWORD = '';
 
-    /**
-     * Show or hide error messages on screen
-     * @var boolean
-     */
     const SHOW_ERRORS = true;
 }
