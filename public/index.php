06559f5c9bcec2edc6316fade2682c5fb4e0568a
linh nguyen <jonathanrocrach2@gmail.com>
2020-05-19 20:18:44 +0700

    fix auth routh

--- public/index.php
+++ public/index.php
@@ -19,7 +19,7 @@ set_exception_handler('Core\Error::exceptionHandler');
 $router = new Core\Router();
 
 // Add the routes
-$router->add('', ['controller' => 'Home', 'action' => 'index']);
+$router->add('', ['controller' => 'Authentication', 'action' => 'index']);
 $router->add('{controller}/{action}');
 $router->add('{controller}/{id:\d+}/{action}');
 $router->add('admin/{controller}/{action}', ['namespace' => 'Admin']);
